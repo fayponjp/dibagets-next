5ca1e46f80f22662d976a8025031b6c90ed32ad4
fayponjp <fayponjp@gmail.com>
2026-03-10 16:08:46 +0800

    updated api and related components for displaying expressions and definitions
    - dictionary.ts renamed and modified: added page count and paginated query
    - added search functionality for header-search component
    - added search clear handling

---
+++ app/api/dictionary.ts
@@ -0,0 +1,61 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+);
+
+export interface Word {
+    wordID: number;
+    word: string;
+    definition: { definition: string }[];
+    created_at: string;
+    example: { example: string }[];
+    tags?: string[];
+}
+
+export interface NewWord {
+    word: string;
+    definition: string;
+    example: string;
+}
+
+export const DictionaryAPI = {
+    async getWords(expression?: string) {
+        let query = supabase
+            .from('Words')
+            .select(
+                'word, tags, wordID, definition:WordDefinitions(definition), example:WordDefinitions(example), created_at',
+            );
+
+        if (expression) {
+            query = query.ilike('word', `%${expression}%`);
+        }
+        const { data, count, error } = await query;
+
+        return { data, error };
+    },
+
+    async getWordsByRange(page: number) {
+        const itemsPerPage = 10;
+        const startingIndex = (page - 1) * itemsPerPage;
+        const endingIndex = page * itemsPerPage - 1;
+        let query = supabase
+            .from('Words')
+            .select(
+                'word, tags, wordID, definition:WordDefinitions(definition), example:WordDefinitions(example), created_at',
+            )
+            .range(startingIndex, endingIndex);
+
+        const { data, error } = await query;
+
+        return { data, error }
+    },
+
+    async getNumberOfRecords() {
+        const query = supabase.from('expressiondictionary').select('*', { count: 'exact', head: true })
+        const { count, error } = await query
+
+        return { count, error }
+    }
+};
